b64823ea69c01df9afed6a255b092a4c60962338
rasta <121708131+mbrasta@users.noreply.github.com>
2025-03-07 19:16:14 +0330

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+setTimeout(function() {
+    document.getElementById("speechBubble").classList.add("hidden");
+}, 5000); // 5000ms = 5 seconds
+=======
+setTimeout(function() {
+    document.getElementById("speechBubble").classList.add("hidden");
+}, 5000); // 5000ms = 5 seconds
+>>>>>>> 9e7cfee (Initial commit)
